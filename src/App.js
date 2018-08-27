3c24bb7fbffc07d129f5a187530f02d472ea83d0
Tim Lee <leewtim@gmail.com>
2018-08-27 15:31:53 -0700

    Added Food2Fork API functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Form from "./components/Form";
+import Recipes from "./components/Recipes";
 import './App.css';
 
+const API_KEY = "d6b8f42f0637ee9f442f9ee051277a05";
+
 class App extends Component {
+
+  state = {
+    recipes: []
+  }
+
+  getRecipe = async (e) => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`);
+    // document.getElementById("form").reset();
+    const data = await api_call.json();
+    this.setState({ recipes:data.recipes });
+    console.log(this.state.recipes);
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Recipe Search</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Form getRecipe={this.getRecipe}/>
+        <Recipes recipes={this.state.recipes}/>
       </div>
     );
   }
